a82b516b5ce5b33253fb04fe69d826c681305695
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-07-15 18:40:43 +0200

    add StackView template

---
+++ tip-calculator/view/BillInputView.swift
@@ -0,0 +1,24 @@
+//
+//  BillInputView.swift
+//  tip-calculator
+//
+//  Created by Caleb Danielsen on 15.07.2023.
+//
+
+import UIKit
+
+class BillInputView: UIView {
+    init() {
+        super.init(frame: .zero)
+        layout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func layout() {
+        backgroundColor = .green
+    }
+}
