2a9479901969b1041ebf8a995369204a62c4383a
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-07-15 19:02:08 +0200

    add font theme

--- tip-calculator/config/ThemeFont.swift
+++ tip-calculator/config/ThemeFont.swift
@@ -6,3 +6,18 @@
 //
 
 import UIKit
+
+struct ThemeFont {
+    // AvenirNext
+    static func regular(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Avenir Next Regular", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func bold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Avenir Next Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+    
+    static func demiBold(ofSize size: CGFloat) -> UIFont {
+        return UIFont(name: "Avenir Next Demi Bold", size: size) ?? .systemFont(ofSize: size)
+    }
+}
