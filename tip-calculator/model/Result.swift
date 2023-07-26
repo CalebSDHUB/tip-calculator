88a9ac67701710e13a3f1f899b44de5978e8a901
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-07-26 23:39:46 +0200

    add view model

---
+++ tip-calculator/model/Result.swift
@@ -0,0 +1,12 @@
+//
+//  Result.swift
+//  tip-calculator
+//
+//  Created by Caleb Danielsen on 26.07.2023.
+//
+
+struct Result {
+    let amountPerPerson: Double
+    let totalBill: Double
+    let totalTip: Double
+}
