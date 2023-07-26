dccce1b01783acec2db176dddeb58d2da38d6708
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-07-26 22:40:56 +0200

    add tip input view

---
+++ tip-calculator/model/Tip.swift
@@ -0,0 +1,29 @@
+//
+//  Tip.swift
+//  tip-calculator
+//
+//  Created by Caleb Danielsen on 26.07.2023.
+//
+
+enum Tip {
+    case none
+    case tenPercent
+    case fiftenPencent
+    case twentyPercent
+    case custom(value: Int)
+    
+    var stringValue: String {
+        switch self {
+        case .none:
+            return ""
+        case .tenPercent:
+            return "10%"
+        case .fiftenPencent:
+            return "15%"
+        case .twentyPercent:
+            return "20%"
+        case .custom(value: let value):
+            return String(value)
+        }
+    }
+}
