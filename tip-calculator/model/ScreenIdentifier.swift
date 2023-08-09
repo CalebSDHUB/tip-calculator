e2965c9f9c3633291ad6f218703eab83494faca6
Caleb Danielsen <calebclientmanifold@gmail.com>
2023-08-09 21:55:46 +0200

    add UI Tests

---
+++ tip-calculator/model/ScreenIdentifier.swift
@@ -0,0 +1,14 @@
+//
+//  ScreenIdentifier.swift
+//  tip-calculator
+//
+//  Created by Caleb Danielsen on 09.08.2023.
+//
+
+enum ScreenIdentifier {
+    enum ResultView: String {
+        case totalAmountPerPersonValueLabel
+        case totalBillValueLabel
+        case totalTipValueLabel
+    }
+}
